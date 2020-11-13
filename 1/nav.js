568458761f4a5527c3025e9dda982cdf835a0ac8
Chunghua1216 <daniel.lee901216@gmail.com>
2020-11-13 14:47:43 +0800

    Initial commit,saving 1~3files

---
+++ 1/nav.js
@@ -0,0 +1,38 @@
+function change(index) {
+  const p = document.getElementById('show');
+  console.log('p', p);
+  switch (index) {
+    case 1:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w01-intro_pokemon/intro.html"/>';
+      break;
+
+    case 2:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w01-intro_pokemon/pokemon.html"/>';
+      break;
+
+    case 3:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w02-Blog/blog.html"/>';
+      break;
+
+    case 4:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w02-ImageGallery/imageegallery.html"/>';
+      break;
+
+    case 5:
+      p.innerHTML = '<iframe width=100% height=100% src="./w03-div/div.html"/>';
+      break;
+
+    case 6:
+      p.innerHTML =
+        '<iframe width=100% height=100% src="./w03-div-tku60/div_tku60.html"/>';
+      break;
+
+    case 7:
+      p.innerHTML = '<iframe width=100% height=100% src="./w06/index.html"/>';
+      break;
+  }
+}
